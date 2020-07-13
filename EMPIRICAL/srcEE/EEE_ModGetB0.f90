161d1216d7de946a85930a333e8d519b6c737d2a
Igor V. Sokolov <igorsok@umich.edu>
2020-07-13 18:10:35 -0400

    modified:   EMPIRICAL/srcEE/EEE_ModGetB0.f90 Skip calculation at TimeSimulation==StartTime, when some quantities are not yet defined, while dynamic B0 is still zero.

--- EMPIRICAL/srcEE/EEE_ModGetB0.f90
+++ EMPIRICAL/srcEE/EEE_ModGetB0.f90
@@ -15,7 +15,7 @@ contains
 
   subroutine EEE_get_B0(x_D,B0_D, TimeSimulation)
 
-    use EEE_ModCommonVariables, ONLY: UseArch, UseTD
+    use EEE_ModCommonVariables, ONLY: UseArch, UseTD, StartTime
     use EEE_ModArch,            ONLY: get_arch
     use EEE_ModTD99,            ONLY: compute_TD99_BqField
     real, intent(in)  :: x_D(3)
@@ -31,7 +31,7 @@ contains
        call get_arch(x_D,B_D)
 
        B0_D = B0_D + B_D
-    elseif(UseTD)then
+    elseif(UseTD.and.TimeSimulation>StartTime)then
        call compute_TD99_BqField(x_D, B_D, TimeSimulation)
        B0_D = B0_D + B_D
     end if
