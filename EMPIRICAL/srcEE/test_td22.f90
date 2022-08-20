0c5a623cc0bdf14f7920742d8e4da50fc542e1fe
Igor V. Sokolov <igorsok@umich.edu>
2022-08-20 05:15:08 -0400

    Move everything related to TD from share libraary to ModTD99

---
+++ EMPIRICAL/srcEE/test_td22.f90
@@ -0,0 +1,10 @@
+!  Copyright (C) 2002 Regents of the University of Michigan,
+!  portions used with permission
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
+program test_td22
+  use ModFieldGS, ONLY:test
+  implicit none
+  !----------------------------------------------------------------------------
+  call test
+end program test_td22
+!==============================================================================
