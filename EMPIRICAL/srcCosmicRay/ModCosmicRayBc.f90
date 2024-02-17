90531f438bf0838d52ad6e548ee0369addfd622b
lwh1106 <whliu@umich.edu>
2024-02-17 12:58:55 -0500

    Minor changes to the naming in A and Z

--- EMPIRICAL/srcCosmicRay/ModCosmicRayBc.f90
+++ EMPIRICAL/srcCosmicRay/ModCosmicRayBc.f90
@@ -39,7 +39,7 @@ contains
     ! and modified by Usoskin et al. 2005 (doi: 10.1029/2005JA011250)
     ! Here we refer to Eq. (2) in Usoskin et al. 2005
 
-    use ModConst, ONLY: cGEV, cLightSpeed, cProtonMass,         &
+    use ModConst, ONLY: cGEV, cLightSpeed, cProtonMass, &
          cElectronCharge, cRMEProton
     ! INPUTS
     integer, intent(in) :: nP  ! The number of meshes in the logarithmic grid
@@ -54,25 +54,25 @@ contains
     real :: EnergySi_I(1:nP), RigiditySi_I(1:nP) ! E_K and R(E_K) in SI units
     real :: RigidityGV_I(1:nP)                   ! R(E_K) in the unit of GV
     real :: DistTimesP2Gn_I(1:nP) ! Distribution function*p**2, [.../(GeV/nuc)]
-    real :: ADefault, ZDefault    ! Default values for A and Z if not present
+    real :: Ai, Zi                ! Default values for A and Z if not present
     !--------------------------------------------------------------------------
-    ADefault = 1.0; ZDefault = 1.0
-    if(present(A)) ADefault = A
-    if(present(Z)) ZDefault = Z
+    Ai = 1.0; Zi = 1.0
+    if(present(A)) Ai = A
+    if(present(Z)) Zi = Z
 
     ! p[Si] -> E_K[Si]
-    EnergySi_I = sqrt((MomentumSi_I * cLightSpeed)**2 +         &
-         (ADefault * cProtonMass * cLightSpeed**2)**2)
+    EnergySi_I = sqrt((MomentumSi_I * cLightSpeed)**2 + &
+         (Ai * cProtonMass * cLightSpeed**2)**2)
     ! E_K[Si] -> R(E_K)[Si]
-    RigiditySi_I = ADefault / abs(ZDefault*cElectronCharge) *   &
+    RigiditySi_I = Ai / abs(Zi*cElectronCharge) *       &
          sqrt(EnergySi_I*(EnergySi_I+2*cRMEProton))
     ! R(E_K)[Si] -> R(E_K)[GV]
     RigidityGV_I = RigiditySi_I / cGEV
     ! R(E_K)[GV] -> j_LIS[.../(GeV/nuc)]
-    DistTimesP2Gn_I = 1.9E+4 * RigidityGV_I**(-2.78) /          &
-         (1 + 0.4866 * RigidityGV_I**(-2.51))
+    DistTimesP2Gn_I = 1.9E+4 * RigidityGV_I**(-2.78) /  &
+         (1.0 + 0.4866 * RigidityGV_I**(-2.51))
     ! j_LIS[.../(GeV/nuc)] -> p^2*f|_{infty}[Si]
-    DistTimesP2Si_I = DistTimesP2Gn_I/(ADefault * cGEV)
+    DistTimesP2Si_I = DistTimesP2Gn_I/(Ai * cGEV)
   end subroutine local_interstellar_spectrum_a
   !============================================================================
 end module ModTransitionRegion
