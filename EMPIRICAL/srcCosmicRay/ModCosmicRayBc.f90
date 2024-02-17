7b709d1cc230d92e35b29e4e28124b46e9c2b800
lwh1106 <whliu@umich.edu>
2024-02-17 12:42:13 -0500

    Implement Eq.(2) in Usoskin et al. 2005 for the LISM flux

--- EMPIRICAL/srcCosmicRay/ModCosmicRayBc.f90
+++ EMPIRICAL/srcCosmicRay/ModCosmicRayBc.f90
@@ -10,27 +10,10 @@ module ModTransitionRegion
   end interface local_interstellar_spectrum
 contains
   !============================================================================
-  subroutine local_interstellar_spectrum_a(nP,MomentumSi_I, XyzSi_D, & ! Input
-       DistTimesP2Si_I,                                              & ! Output
-       A, Z)                                                         ! Optional
-
-    ! INPUTS
-    integer, intent(in) :: nP  ! The number of meshes in the logarithmic grid
-    real, intent(in) :: MomentumSi_I(1:nP)   ! Particle momenta in Si units
-    real, intent(in) :: XyzSi_D(3)           ! Where to set BC
-    ! OUTPUT
-    real, intent(out):: DistTimesP2Si_I(1:nP) ! Distribution function*p**2, Si
-    ! OPTIONAL
-    real, OPTIONAL, intent(in) :: A          ! To convert to energy per nuc
-    real, OPTIONAL, intent(in) :: Z          ! To convert to rigidity
-    !----------------------------
-  end subroutine local_interstellar_spectrum_a
-  !============================================================================
   subroutine local_interstellar_spectrum_s(MomentumSi, XyzSi_D,    & ! Input
        DistTimesP2Si,                                              & ! Output
        A, Z)                                                         ! Optional
 
-    use ModConst, ONLY: cGEV
     ! INPUTS
     real, intent(in) :: MomentumSi    ! Particle momentum in Si units
     real, intent(in) :: XyzSi_D(3)    ! Where to set BC
@@ -41,12 +24,56 @@ contains
     real, OPTIONAL, intent(in) :: Z   ! To convert to rigidity
     ! LOCAL VARS
     real :: FakeMomentumArr_I(1), FakeDistTimesP2_I(1)
-    !----------------------------
+    !--------------------------------------------------------------------------
     FakeMomentumArr_I(1) = MomentumSi
     call local_interstellar_spectrum_a(1,FakeMomentumArr_I, XyzSi_D, & ! Input
          FakeDistTimesP2_I,                                          & ! Output
          A, Z)                                                       ! Optional
     DistTimesP2Si = FakeDistTimesP2_I(1)
   end subroutine local_interstellar_spectrum_s
-  !============================================================
+  !============================================================================
+  subroutine local_interstellar_spectrum_a(nP,MomentumSi_I, XyzSi_D, & ! Input
+       DistTimesP2Si_I,                                              & ! Output
+       A, Z)                                                         ! Optional
+    ! Formulae for LISM flux by Burger et al. 2000 (doi: 10.1029/2000JA000153)
+    ! and modified by Usoskin et al. 2005 (doi: 10.1029/2005JA011250)
+    ! Here we refer to Eq. (2) in Usoskin et al. 2005
+
+    use ModConst, ONLY: cGEV, cLightSpeed, cProtonMass,         &
+         cElectronCharge, cRMEProton
+    ! INPUTS
+    integer, intent(in) :: nP  ! The number of meshes in the logarithmic grid
+    real, intent(in) :: MomentumSi_I(1:nP)   ! Particle momenta in Si units
+    real, intent(in) :: XyzSi_D(3)           ! Where to set BC
+    ! OUTPUT
+    real, intent(out):: DistTimesP2Si_I(1:nP) ! Distribution function*p**2, Si
+    ! OPTIONAL
+    real, OPTIONAL, intent(in) :: A          ! To convert to energy per nuc
+    real, OPTIONAL, intent(in) :: Z          ! To convert to rigidity
+    ! LOCAL VARS
+    real :: EnergySi_I(1:nP), RigiditySi_I(1:nP) ! E_K and R(E_K) in SI units
+    real :: RigidityGV_I(1:nP)                   ! R(E_K) in the unit of GV
+    real :: DistTimesP2Gn_I(1:nP) ! Distribution function*p**2, [.../(GeV/nuc)]
+    real :: ADefault, ZDefault    ! Default values for A and Z if not present
+    !--------------------------------------------------------------------------
+    ADefault = 1.0; ZDefault = 1.0
+    if(present(A)) ADefault = A
+    if(present(Z)) ZDefault = Z
+
+    ! p[Si] -> E_K[Si]
+    EnergySi_I = sqrt((MomentumSi_I * cLightSpeed)**2 +         &
+         (ADefault * cProtonMass * cLightSpeed**2)**2)
+    ! E_K[Si] -> R(E_K)[Si]
+    RigiditySi_I = ADefault / abs(ZDefault*cElectronCharge) *   &
+         sqrt(EnergySi_I*(EnergySi_I+2*cRMEProton))
+    ! R(E_K)[Si] -> R(E_K)[GV]
+    RigidityGV_I = RigiditySi_I / cGEV
+    ! R(E_K)[GV] -> j_LIS[.../(GeV/nuc)]
+    DistTimesP2Gn_I = 1.9E+4 * RigidityGV_I**(-2.78) /          &
+         (1 + 0.4866 * RigidityGV_I**(-2.51))
+    ! j_LIS[.../(GeV/nuc)] -> p^2*f|_{infty}[Si]
+    DistTimesP2Si_I = DistTimesP2Gn_I/(ADefault * cGEV)
+  end subroutine local_interstellar_spectrum_a
+  !============================================================================
 end module ModTransitionRegion
+!==============================================================================
