4b821f54c7a03fa60de5467ba36549e4f57f8d7d
lwh1106 <whliu@umich.edu>
2024-02-22 13:01:42 -0500

    Declare TypeLisBc in ModCosmicRay as a public variable and read param from ModBc

--- EMPIRICAL/srcCosmicRay/ModCosmicRayBc.f90
+++ EMPIRICAL/srcCosmicRay/ModCosmicRayBc.f90
@@ -8,11 +8,13 @@ module ModCosmicRay
      module procedure local_interstellar_spectrum_s
      module procedure local_interstellar_spectrum_a
   end interface local_interstellar_spectrum
+
+  character(LEN=18), public :: TypeLisBc = 'default' ! Decide which LIS to use
 contains
   !============================================================================
   subroutine local_interstellar_spectrum_s(MomentumSi, XyzSi_D,    & ! Input
        DistTimesP2Si,                                              & ! Output
-       TypeLisBcIn, A, Z)                                          ! Optional
+       A, Z)                                                       ! Optional
 
     ! INPUTS
     real, intent(in) :: MomentumSi    ! Particle momentum in Si units
@@ -22,20 +24,19 @@ contains
     ! OPTIONAL
     real, OPTIONAL, intent(in) :: A   ! To convert to energy per nuc
     real, OPTIONAL, intent(in) :: Z   ! To convert to rigidity
-    character(LEN=18),OPTIONAL,intent(in):: TypeLisBcIn ! Decide which LIS
     ! LOCAL VARS
     real :: FakeMomentumArr_I(1), FakeDistTimesP2_I(1)
     !--------------------------------------------------------------------------
     FakeMomentumArr_I(1) = MomentumSi
     call local_interstellar_spectrum_a(1,FakeMomentumArr_I, XyzSi_D, & ! Input
          FakeDistTimesP2_I,                                          & ! Output
-         TypeLisBcIn, A, Z)                                          ! Optional
+         A, Z)                                                       ! Optional
     DistTimesP2Si = FakeDistTimesP2_I(1)
   end subroutine local_interstellar_spectrum_s
   !============================================================================
   subroutine local_interstellar_spectrum_a(nP,MomentumSi_I, XyzSi_D, & ! Input
        DistTimesP2Si_I,                                              & ! Output
-       TypeLisBcIn, A, Z)                                            ! Optional
+       A, Z)                                                         ! Optional
     ! Formulae for LISM flux by Burger et al. 2000 (doi: 10.1029/2000JA000153)
     ! and modified by Usoskin et al. 2005 (doi: 10.1029/2005JA011250)
     ! Here we refer to Eq.(2) in Usoskin et al. 2005 as default
@@ -53,9 +54,7 @@ contains
     ! OPTIONAL
     real, OPTIONAL, intent(in) :: A          ! To convert to energy per nuc
     real, OPTIONAL, intent(in) :: Z          ! To convert to rigidity
-    character(LEN=18),OPTIONAL,intent(in):: TypeLisBcIn ! Decide which LIS
     ! LOCAL VARS
-    character(LEN=18) :: TypeLisBc               ! Decide LIS to use finally
     real :: EnergySi_I(1:nP)                     ! E_K and R(E_K) in SI units
     real :: RigidityGv_I(1:nP)                   ! R(E_K) in the unit of GV
     real :: DistTimesP2Gn_I(1:nP) ! Distribution function*p**2, [.../(GeV/nuc)]
@@ -76,12 +75,6 @@ contains
     ! E_K|_[Si] [J] -> R(E_K)|_[Si] [GV]
     RigidityGv_I = 1.0/abs(Zi*cGeV)*                   &
          sqrt(EnergySi_I*(EnergySi_I+2*cRmeProton))
-
-    if(present(TypeLisBcIn)) then
-       TypeLisBc = TypeLisBcIn
-    else
-       TypeLisBc = 'default'
-    end if
 
     select case(TypeLisBc)
     case('default', 'usoskin2005')
