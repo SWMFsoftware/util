bd176c9b5e774b7c693cda67678ec232e0880196
lwh1106 <whliu@umich.edu>
2024-02-23 20:04:48 -0500

    Add ModulationPotential for GCR spectrum at ~1 AU

--- EMPIRICAL/srcCosmicRay/ModCosmicRayBc.f90
+++ EMPIRICAL/srcCosmicRay/ModCosmicRayBc.f90
@@ -10,6 +10,8 @@ module ModCosmicRay
   end interface local_interstellar_spectrum
 
   character(LEN=18), public :: TypeLisBc = 'default' ! Decide which LIS to use
+  logical, public :: UseModulationPotential = .false.
+  real, public :: ModulationPotential = 0.0
 contains
   !============================================================================
   subroutine local_interstellar_spectrum_s(MomentumSi, XyzSi_D,    & ! Input
@@ -50,15 +52,18 @@ contains
     real, intent(in) :: MomentumSi_I(1:nP)   ! Particle momenta in Si units
     real, intent(in) :: XyzSi_D(3)           ! Where to set BC
     ! OUTPUT
-    real, intent(out):: DistTimesP2Si_I(1:nP) ! Distribution function*p**2, Si
+    real, intent(out):: DistTimesP2Si_I(1:nP)! Distribution function*p**2, Si
     ! OPTIONAL
     real, OPTIONAL, intent(in) :: A          ! To convert to energy per nuc
     real, OPTIONAL, intent(in) :: Z          ! To convert to rigidity
     ! LOCAL VARS
-    real :: EnergySi_I(1:nP)                     ! E_K and R(E_K) in SI units
-    real :: RigidityGv_I(1:nP)                   ! R(E_K) in the unit of GV
+    real :: EnergyGn_I(1:nP)                 ! E_K/A in Gev per nucleon
+    real :: RigidityGv_I(1:nP)               ! R(E_K/A) in the unit of GV
     real :: DistTimesP2Gn_I(1:nP) ! Distribution function*p**2, [.../(GeV/nuc)]
+    real :: EnergyLossGn                     ! Phi=Z*e/A*phi, avg. energy loss
+    real :: DistLisToUpperBc_I(1:nP)         ! Factor: LIS -> ~1 AU Spectrum
     real :: Ai, Zi                ! Default values for A and Z if not present
+    real, parameter :: cRmeProtonGeV = cRmeProton/cGeV
     ! For Eq.(13) in Corti et al., 2019 (doi: 10.3847/1538-4357/aafac4)
     real, parameter :: nFit = 5658.0, Gamma0Fit = 1.669
     real, parameter :: RigidityFit(3) = [0.572, 6.2, 540.0]
@@ -69,17 +74,25 @@ contains
     if(present(A)) Ai = A
     if(present(Z)) Zi = Z
 
-    ! p|_[Si] -> E_K|_[Si] [J], all nucleons
-    EnergySi_I = sqrt((MomentumSi_I*cLightSpeed)**2 +  &
-         (Ai*cRmeProton)**2) - Ai*cRmeProton
-    ! E_K|_[Si] [J] -> R(E_K)|_[Si] [GV]
-    RigidityGv_I = 1.0/abs(Zi*cGeV)*                   &
-         sqrt(EnergySi_I*(EnergySi_I+2*cRmeProton))
+    ! Given ModulationPotential (phi[MV]), the averaged energy loss of
+    ! cosmic rays inside the heliosphere is given by Phi = (Z*e/A)*phi
+    ! It equals to Zi/Ai * ModulationPotential * cMeV/cGeV to get [GeV/nuc]
+    EnergyLossGn = Zi/Ai * ModulationPotential * 1.0E-3
+
+    ! p|_[Si] -> E_K/A [GeV/nuc]
+    EnergyGn_I = (sqrt((MomentumSi_I*cLightSpeed)**2 + &
+         (Ai*cRmeProton)**2) - Ai*cRmeProton)/(cGeV*Ai)
+    ! E_K/A [GeV/nuc] -> R(E_K/A) [GV]
+    RigidityGv_I = Ai/abs(Zi)*sqrt(EnergyGn_I*(EnergyGn_I+2*cRmeProtonGeV))
 
     select case(TypeLisBc)
     case('default', 'usoskin2005')
        ! For Eq.(2) in Usoskin et al. 2005 (doi: 10.1029/2005JA011250)
-       ! R(E_K)[GV] -> j_LIS[.../(GeV/nuc)]
+       ! If UseModulationPotential: we need j_LIS(R(E_K/A + Phi))
+       if (UseModulationPotential) RigidityGv_I =      &
+            Ai/abs(Zi)*sqrt((EnergyGn_I+EnergyLossGn)* &
+            ((EnergyGn_I+EnergyLossGn)+2*cRmeProtonGeV))
+       ! R(E_K/A + Phi)[GV] -> j_LIS[.../(GeV/nuc)]
        DistTimesP2Gn_I = 1.9E+4*RigidityGv_I**(-2.78)/ &
             (1.0+0.4866*RigidityGv_I**(-2.51))
        ! j_LIS[.../(GeV/nuc)] -> p^2*f|_{infty}[Si]
@@ -94,15 +107,26 @@ contains
             (1.0+RigidityFit(2)**(-sFit(2))))**(DeltaFit(2)/sFit(2)) *     &
             ((1.0+(RigidityGv_I/RigidityFit(3))**sFit(3))/                 &
             (1.0+RigidityFit(3)**(-sFit(3))))**(DeltaFit(3)/sFit(3)) *     &
-            Ai/abs(Zi*cElectronCharge)*(EnergySi_I+cRmeProton)/            &
-            sqrt(EnergySi_I*(EnergySi_I+2*cRmeProton))
+            Ai/abs(Zi)*(EnergyGn_I+cRmeProtonGeV)/                         &
+            sqrt(EnergyGn_I*(EnergyGn_I+2*cRmeProtonGeV))
        ! j_LIS[.../(GeV/nuc)] -> p^2*f|_{infty}[Si]
        DistTimesP2Si_I = DistTimesP2Gn_I/(Ai*cGeV)
     case default
        call CON_stop('Unknown type of LIS of: '//TypeLisBc)
     end select
 
-    ! All in all, this may work only without modulation potential, i.e., LIS
+    ! DistLisToUpperBc_I: Set UpperEndBc_I as LIS or GCR Spectrum at ~1 AU
+    if(UseModulationPotential) then
+       DistLisToUpperBc_I = 1.0
+    else
+       ! Theoretical basis: the force field approximation in
+       ! Gleeson and Axford [1968] and McCracken et al. [2004a].
+       DistLisToUpperBc_I = EnergyGn_I*(EnergyGn_I+2*cRmeProtonGeV)/      &
+            ((EnergyGn_I+EnergyLossGn)*                                   &
+            (EnergyGn_I+EnergyLossGn+2*cRmeProtonGeV))
+    end if
+    ! Finally stick to LIS or use the spectrum in inner heliosphere
+    DistTimesP2Si_I = DistTimesP2Si_I*DistLisToUpperBc_I
 
   end subroutine local_interstellar_spectrum_a
   !============================================================================
